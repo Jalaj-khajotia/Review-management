68bbe28f9a5191841c0cc77203d69a59b6e75170
jalaj <jalajiitr@gmail.com>
2016-01-24 16:02:33 +0530

    search bar updated

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,4 +8,39 @@
  */
 angular.module('sbAdminApp')
   .controller('MainCtrl', function($scope,$position) {
+  	  $scope.myFunction = function() {
+        document.getElementById("myDropdown").classList.toggle("show");
+       // $scope.dropdownClicked();
+    }
+
+    // Close the dropdown menu if the user clicks outside of it
+/*    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }*/
+    $scope.isDropDownVisible = false;
+
+    $scope.OptionSelected = function(val){
+    	$scope.isDropDownVisible = false;
+    	if(val == "1"){
+    		$scope.searchOption = "by Name";    		
+    	} else if(val == "2"){
+    		$scope.searchOption = "by Order id";
+    	}else if(val == "3"){
+    		$scope.searchOption = "by Mobile";
+    	}
+    	console.log(val);
+    }
+    $scope.dropdownClicked = function(){
+    	 $scope.isDropDownVisible = !  $scope.isDropDownVisible;
+    }
   });
